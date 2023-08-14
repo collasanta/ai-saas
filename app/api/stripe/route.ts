d8be1831f3bdace72d04630efb7abcfe18f27b17
Victor Henrique Collasanta <victor.collasanta@gmail.com>
2023-08-14 20:51:39 -0300

    fix price to 10 dol

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -45,7 +45,7 @@ export async function GET() {
                             description:"Add 1000 credits (ONE TIME PAYMENT)",
                             images:["https://chadchapters.com/logo.png"],
                         },
-                        unit_amount: 600,
+                        unit_amount: 1000,
                     },
                     quantity: 1,
                 }
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,4 @@
 import { authMiddleware } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
-import { env } from "process";
-
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 
 export default authMiddleware({
     publicRoutes:["/", "/bot", "/api/webhook"],
